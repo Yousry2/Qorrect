497cd0546cafd45a5e067781213fb7bfd9745fcb
Mohamed Yousry <mohammed.yousry@thoughtdesign.com.au>
2020-06-14 22:10:58 +0200

    develop : add app reducers structure

---
+++ src/app/app.reducer.ts
@@ -0,0 +1,10 @@
+import { ActionReducerMap, createFeatureSelector, createSelector } from '@ngrx/store';
+
+
+export interface State {
+}
+
+export const reducers: ActionReducerMap<State> = {
+};
+
+
